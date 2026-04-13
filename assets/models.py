694d1288fdd433157b6ed161565b6471b98e60ec
Nicolas <ni_colasaires@hotmail.com>
2026-04-13 16:11:42 -0300

    chore: Add self identifier @assets.models

--- assets/models.py
+++ assets/models.py
@@ -5,3 +5,6 @@ class Stock(models.Model):
     name = models.CharField(null=False, blank=False, max_length=55)
     ticker = models.CharField(null=False, blank=False, max_length=55)
     last_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
