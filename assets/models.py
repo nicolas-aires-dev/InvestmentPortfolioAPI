f5299ae890cc5a37f3995dc267e86bede0ba5508
Nicolas <ni_colasaires@hotmail.com>
2026-04-13 15:33:32 -0300

    feat: Add Stock model.

---
+++ assets/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# name, ticker, last_price
+class Stock(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=55)
+    ticker = models.CharField(null=False, blank=False, max_length=55)
+    last_price = models.DecimalField(max_digits=10, decimal_places=2)
--- settings/settings.py
+++ settings/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
 
     # Apps
     'accounts',
+    'assets',
 ]
 
 MIDDLEWARE = [
