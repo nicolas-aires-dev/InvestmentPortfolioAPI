b092c90389ad6bbc65e806f15f97cf03fa82d7cd
Nicolas <ni_colasaires@hotmail.com>
2026-04-13 16:08:03 -0300

    feat(models): Add Portfolio models.

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Portfolio(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT, related_name='portfolio')
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
